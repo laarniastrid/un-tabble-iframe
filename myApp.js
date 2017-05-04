0fcd2fe4fab568eb3fd42514d5b86d02a22b0fe5
Astrid Sutton <astrid.sutton@wgu.edu>
2017-05-04 09:41:45 -0600

    commented out preferred controller structure, added comment about preferred

--- myApp.js
+++ myApp.js
@@ -14,19 +14,26 @@ function iframeNoTab() {
 }
 
 
-(function() {
-    angular.module('myApp')
-        .controller('iframeNoTab', iframeNoTab);
 
-    iframeNoTab.$inject = [];
+/**
+ * Below : this is how I prefer to make controllers, but for some reason codepen
+ * didn't seem to like it.  Also, I didn't spend much time on why it wasn't working
+ * this way on codepen since it was such a small test.
+ */
 
-    function iframeNoTab() {
-        var self = this;
+// (function() {
+//     angular.module('myApp')
+//         .controller('iframeNoTab', iframeNoTab);
+
+//     iframeNoTab.$inject = [];
+
+//     function iframeNoTab() {
+//         var self = this;
         
-        self.stopClick = function() {
-            alert('hi there');
-        }
+//         self.stopClick = function() {
+//             alert('hi there');
+//         }
 
-        self.test = 'top - outside of iframe';
-    }
-})();
+//         self.test = 'top - outside of iframe';
+//     }
+// })();
